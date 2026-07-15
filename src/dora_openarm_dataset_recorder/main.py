771b7951de4caed41eaca010f341efc09746c17c
Kohei SENDAI <80389896+k1000dai@users.noreply.github.com>
2026-07-15 11:54:06 +0900

    bump v4 (#36)

--- src/dora_openarm_dataset_recorder/main.py
+++ src/dora_openarm_dataset_recorder/main.py
@@ -173,7 +173,7 @@ class EpisodeWriter:
 class DatasetWriter:
     """Write a dataset."""
 
-    _VERSION = "0.3.0"
+    _VERSION = "0.4.0"
 
     def __init__(self, directory, name, metadata):
         """Initialize variables."""
